94ea90062b9d0a65b64abe5c15670998c21cedd2
上田　直樹 <69281143+naoki51931@users.noreply.github.com>
2026-09-23 14:18:10 +0900

    Expose block generation and desktop writing tools

--- desktop/preload.js
+++ desktop/preload.js
@@ -1,8 +1,13 @@
 const { contextBridge, ipcRenderer } = require("electron");
-
 contextBridge.exposeInMainWorld("lexis", {
   getSettings: () => ipcRenderer.invoke("settings:get"),
   saveSettings: (settings) => ipcRenderer.invoke("settings:save", settings),
+  listModels: (input) => ipcRenderer.invoke("models:list", input),
   generateNovel: (input) => ipcRenderer.invoke("novel:generate", input),
+  generateBlocks: (input) => ipcRenderer.invoke("novel:generate-blocks", input),
+  continueNovel: (input) => ipcRenderer.invoke("novel:continue", input),
+  getHistory: () => ipcRenderer.invoke("history:get"),
+  saveTemplate: (template) => ipcRenderer.invoke("templates:save", template),
+  getTemplates: () => ipcRenderer.invoke("templates:get"),
   saveNovel: (payload) => ipcRenderer.invoke("novel:save", payload)
 });
